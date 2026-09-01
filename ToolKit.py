e52cb90826bbf960db00c9408fef37571d7d3c5f
Joy-Majumder <gopalmajumdarjoy@gmail.com>
2026-09-01 10:58:34 +0600

    ToolKit Baddie v3

--- ToolKit.py
+++ ToolKit.py
@@ -1,8 +1,7 @@
 import cv2 as cv
-import os
+
 def grayscale_image():
-    ok = os.getcwd()
-    path = os.path.join(ok, '/Users/joy0x1/Downloads/UIU Mariner/Assignment/Ass2/ass/im1.jpeg')
+    path = "/Users/joy0x1/Downloads/UIU Mariner/Assignment/Ass2/ass/im1.jpeg"
     img = cv.imread(path)
     imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # this one gonna be used for gray color -,-
     # imgray = cv.cvtColor(img, cv.COLOR_BGR2RGB) # this one gonna be used for rbg color
