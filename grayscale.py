77059ac5de8595034b7b6623d6d445023470c506
Joy-Majumder <gopalmajumdarjoy@gmail.com>
2026-09-01 10:16:52 +0600

    GrayScale Baddie 0.2

--- grayscale.py
+++ grayscale.py
@@ -4,12 +4,14 @@ def grayscale_image():
     ok = os.getcwd()
     path = os.path.join(ok, '/Users/joy0x1/Downloads/UIU Mariner/Assignment/Ass2/ass/im1.jpeg')
     img = cv.imread(path)
-    imgray = cv.cvtColor(img, cv.COLOR_BAYER_GR2BGRA)
+    # imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # this one gonna be used for gray color -,-
+    # imgray = cv.cvtColor(img, cv.COLOR_BGR2RGB) # this one gonna be used for rbg color
+    imgray = cv.cvtColor(img, cv.COLOR_BGR2LAB) # this one gonna be used for rbg color
 
     cv.imshow('image', imgray)
     cv.waitKey(0)
 
     print(img.shape)
 
-if __name__ == '__main__':
-    grayscale_image()
+
+grayscale_image()
